d82a67675cb33425e3a93a15e52483d074ec5338
Stephen <stephenjohnsongames@gmail.com>
2021-03-17 20:35:22 +0000

    adding project files

---
+++ Assets/004.ReflectionProbe/Orbit.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+
+    public Transform centrePoint;
+    public bool upAxis = false;
+    public bool rightAxis = false;
+    private Vector3 axis;
+    public float radius = 2.0f;
+    public float radiusSpeed = 0.5f;
+    public float rotationSpeed = 80.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = (transform.position - centrePoint.position).normalized * radius + centrePoint.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(upAxis == true)
+        {
+            axis = Vector3.up;
+        }
+
+        if(rightAxis == true)
+        {
+            axis = Vector3.right;
+        }
+
+        transform.RotateAround(centrePoint.position, axis, rotationSpeed * Time.deltaTime);
+        Vector3 desiredPosition = (transform.position - centrePoint.position).normalized * radius + centrePoint.position;
+        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+    }
+}
---
+++ README.txt
@@ -0,0 +1,29 @@
+Test Case README
+
+- Test cases are made in 2019.4.18f1.
+
+- Each case has a linear scene for each feature, labelled by a number, ready for the fovo packages to be brought in.
+
+- Screenshots for Linear/FOVO are taken at 720p.
+
+
+Additional Notes to be added as more tests are added...
+
+
+NOTES
+
+- Light settings have been changed to remove default skybox and change environment lighting settings to be a colour.
+
+- Screenshots have been taken using forward mode.
+
+- Auto Generate light is on for Test 5 + 6 (Light probe + Baked Scene), please wait for the light to bake before testing
+
+- Extra post-process images are added in Test 8, uses Vignette just to make extra clear post process is on
+
+- LOD Bias may need to be adjusted if issues with Test 9 (LODGroups), can be found in Quality Settings
+
+- Particle shadow scale when changing projections on Test 10?
+
+- Test 11 (Scaling Factor) has different sized cubes imported from Blender, scale each to compare
+
+ 
